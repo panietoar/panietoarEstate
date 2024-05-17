c899ac4a03b531fb20d0d338165af52e41df8964
Pablo Arturo Nieto Arias <PArias3@slb.com>
2024-05-17 10:56:06 -0500

    Added Mongo connection

--- api/index.ts
+++ api/index.ts
@@ -1,10 +1,22 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
+import mongoose from "mongoose";
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT;
+
+const connectionString: string = process.env.MONGO || "";
+
+mongoose
+  .connect(connectionString)
+  .then(() => {
+    console.log("Connected to mongo");
+  })
+  .catch((err: Error) => {
+    console.error(err);
+  });
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Express + TypeScript Server");
